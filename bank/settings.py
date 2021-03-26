b44bef944535e96bf0f85effee9ecff1ae394cc9
Isaiah <isaiaholadapo18@gmail.com>
2021-03-26 04:16:17 +0100

    updated sttings

--- bank/settings.py
+++ bank/settings.py
@@ -141,10 +141,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-#STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
